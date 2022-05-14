4454143e80a5b103867712f2815347bbb786c4b7
magon-miso <m.agonf@uniandes.edu.co>
2022-05-14 13:21:35 -0500

    se incluye registro de usuario admin para tests cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,40 @@
 
+Cypress.Commands.add('createAdmin', (version, site, name, email, password) => {
+    cy.visit('/ghost/')
+    cy.wait(1000)
+    cy.url().then(($url) => { // cy.url().should('include', 'signin')
+        cy.log('url: '+ $url)
+        if ($url.includes('setup')) {
+            // cy.url().should('include', 'setup/')
+            if (version == '3.41.1')
+                cy.visit('/ghost/#/setup/two')
+            else // 4.41.3
+                cy.visit('/ghost')
+    
+            cy.wait(1000)    
+            cy.get('input[id="blog-title"]').clear().type(site)
+            cy.get('input[id="name"]').clear().type(name)
+            cy.get('input[id="email"]').clear().type(email)
+            cy.get('input[id="password"]').clear().type(password)
+            cy.get('button[type="submit"]').click()
+    
+            cy.wait(1000)
+            if (version == '3.41.1') {
+                cy.url().should('include', 'setup/three')
+                cy.get('button[class="gh-flow-skip"]').click()
+                cy.url().should('include', 'site')
+            }
+            else {
+                cy.url().should('include', 'setup/done')
+                cy.visit('/ghost/#/dashboard')        
+            }
+        
+            cy.wait(1000)
+            cy.visit('/ghost/#/signout/')          
+        } 
+    })
+})
+
 Cypress.Commands.add('login', (username, password) => {
     cy.visit('/ghost')
     cy.url().should('include', 'signin')
@@ -6,10 +42,12 @@ Cypress.Commands.add('login', (username, password) => {
     cy.get('input[name="identification"]').clear().type(username)
     cy.get('input[name="password"]').clear().type(password)
     cy.get('button[type="submit"]').click()
+    cy.wait(1000)
 	// cy.screenshot()
 })
 
 Cypress.Commands.add('logout', () => {
+    cy.wait(1000)
     cy.visit('/ghost/#/signout/')
     // cy.url().should('include', 'signin')
 })
@@ -37,16 +75,17 @@ Cypress.Commands.add('newPage', () => {
 })
 
 Cypress.Commands.add('createPage', (version, title, description) => {
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('textarea[placeholder="Page title"]').type(title)
     else
         cy.get('textarea[placeholder="Page Title"]').type(title)
     cy.get('.koenig-editor__editor-wrapper').type(description +'{enter}')
+    cy.wait(1000)
     cy.screenshot()
 })
 
 Cypress.Commands.add('editPage', (version, title, description) => {
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('textarea[placeholder="Page title"]').type(title)
     else
         cy.get('textarea[placeholder="Page Title"]').type(title)
@@ -54,11 +93,12 @@ Cypress.Commands.add('editPage', (version, title, description) => {
     cy.screenshot()
 
     cy.contains('Update').click()
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('button.gh-btn.gh-btn-black.gh-publishmenu-button.gh-btn-icon.ember-view').click()
     else
 	    cy.get('button.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
-	cy.screenshot()		
+	cy.wait(1000)
+    cy.screenshot()		
 })
 
 Cypress.Commands.add('editPageExcerpt', (version, excerpt) => {
@@ -70,16 +110,17 @@ Cypress.Commands.add('editPageExcerpt', (version, excerpt) => {
     cy.get('button.close.settings-menu-header-action').click()
 
     cy.contains('Update').click()
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('button.gh-btn.gh-btn-black.gh-publishmenu-button.gh-btn-icon.ember-view').click()
     else
 	    cy.get('button.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
     // cy.get('.gh-publishmenu-button').click()
+    cy.wait(1000)
     cy.screenshot()
 })
 
 Cypress.Commands.add('editTagPage', (version, tag) => {
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('button[title="Settings"]').click()
     else
         cy.get('button.post-settings').click()
@@ -89,30 +130,32 @@ Cypress.Commands.add('editTagPage', (version, tag) => {
     cy.screenshot()
     // cy.get('.settings-menu-toggle').click()
   
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('button[title="Settings"]').click()
         // cy.get('button.settings-menu-toggle.gh-btn.gh-btn-editor.gh-btn-icon.icon-only.gh-btn-action-icon').click()
     else
         cy.get('button.close.settings-menu-header-action').click()
 
     cy.contains('Update').click()
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('button.gh-btn.gh-btn-black.gh-publishmenu-button.gh-btn-icon.ember-view').click()
     else
         cy.get('button.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
     // cy.get('.gh-publishmenu-button').click()
+    cy.wait(1000)
     cy.screenshot()		
 })
 
 Cypress.Commands.add('deletePage', (version, page) => {
     cy.contains(page).click() 
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('button[title="Settings"]').click() 
     else
         cy.get('button.post-settings').click()
     cy.get('button.gh-btn.gh-btn-hover-red.gh-btn-icon.settings-menu-delete-button').click() 
     cy.screenshot()
     cy.get('.gh-btn.gh-btn-red.gh-btn-icon.ember-view').children().contains('Delete').click()
+    cy.wait(1000)
     // cy.get('.modal-footer .gh-btn:not(:first-child)').click()
 })
 
@@ -121,19 +164,21 @@ Cypress.Commands.add('schedulePage', (version) => {
     cy.get(':nth-child(2) > .gh-publishmenu-radio-button').click()
     cy.get('.gh-publishmenu-footer .gh-btn-icon span').click()
     // cy.get('.modal-footer .gh-btn:not(:first-child)').click()
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('button.gh-btn.gh-btn-black.gh-publishmenu-button.gh-btn-icon.ember-view').click()
     else
         cy.get('button.gh-btn.gh-btn-outline.gh-btn-link').click()
+    cy.wait(2000)
 })
 
 Cypress.Commands.add('publishPage', (version) => {
     cy.contains('Publish').click()
     cy.screenshot()
-    if (version == '4.44')
+    if (version == '4.41.3')
         cy.get('button.gh-btn.gh-btn-black.gh-publishmenu-button.gh-btn-icon.ember-view').click()
     else
         cy.get('button.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
+    cy.wait(1000)
 })
 
 Cypress.Commands.add('featurePage', () => {
